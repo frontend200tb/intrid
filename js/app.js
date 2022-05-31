ac935a4079b107994c3db2a6683874c48909e325
Иван Каширин <85587225+frontend200tb@users.noreply.github.com>
2022-05-31 23:05:20 +0300

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -28,4 +28,53 @@ end Ссылка меню становится активная по клику
 *******************************************/
 
 
+/***************************************
+Анимация (появление блоков при скролле)
+***************************************/
+function onEntry(entry) {
+  entry.forEach(change => {
+    if (change.isIntersecting) {
+      change.target.classList.add('element-show');
+    }
+  });
+}
+
+let options = { threshold: [0.5] };
+let observer = new IntersectionObserver(onEntry, options);
+let elements = document.querySelectorAll('.element-animation');
+for (let elm of elements) {
+  observer.observe(elm);
+}
+/***************************************
+end Анимация (появление блоков при скролле)
+***************************************/
+
+
+/***************************************
+Бургер меню
+***************************************/
+let iconMenu = document.querySelector(".icon-menu");
+if (iconMenu != null) {
+   let delay = 500;
+   let menuBody = document.querySelector(".nav");
+   iconMenu.addEventListener("click", function (e) {
+      if (unlock) {
+         body_lock(delay);
+         iconMenu.classList.toggle("_active");
+         menuBody.classList.toggle("_active");
+      }
+   });
+};
+
+function menu_close() {
+   let iconMenu = document.querySelector(".icon-menu");
+   let menuBody = document.querySelector(".nav");
+   iconMenu.classList.remove("_active");
+   menuBody.classList.remove("_active");
+}
+/***************************************
+end Бургер меню
+***************************************/
+
+
 console.info('frontend200tb intrid end');
