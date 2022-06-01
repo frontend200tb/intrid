684ded50c131feabd5d1f58994f03cc7f17530a4
Иван Каширин <85587225+frontend200tb@users.noreply.github.com>
2022-06-01 22:41:31 +0300

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -30,19 +30,25 @@ end Ссылка меню становится активная по клику
 
 /***************************************
 Анимация (появление блоков при скролле)
+Каждый элемент (с классом animation-element) который должен появляться помещаем в свой контейнер (с классом animation-container) и следим за контейнером. Когда контейнер появляется в окне браузера при скролле - элемент появляется. Когда контейнер исчезает - элемент исчезает, чтобы анимация потом повторилась
 ***************************************/
 function onEntry(entry) {
   entry.forEach(change => {
+    const animElem = change.target.querySelector('.animation-element');
+    //по умолчанию проверяется на пересечение с окном браузера
     if (change.isIntersecting) {
-      change.target.classList.add('element-show');
+      animElem.classList.add('element-show');
+    } else {
+      animElem.classList.remove('element-show');
     }
   });
 }
 
-let options = { threshold: [0.5] };
-let observer = new IntersectionObserver(onEntry, options);
-let elements = document.querySelectorAll('.element-animation');
+//создаем наблюдателя
+let observer = new IntersectionObserver(onEntry);
+let elements = document.querySelectorAll('.animation-container');
 for (let elm of elements) {
+  //сообщаем наблюдателю какие элементы надо отслеживать
   observer.observe(elm);
 }
 /***************************************
@@ -54,26 +60,77 @@ end Анимация (появление блоков при скролле)
 Бургер меню
 ***************************************/
 let iconMenu = document.querySelector(".icon-menu");
+let menuBody = document.querySelector(".nav");
 if (iconMenu != null) {
-   let delay = 500;
-   let menuBody = document.querySelector(".nav");
-   iconMenu.addEventListener("click", function (e) {
-      if (unlock) {
-         body_lock(delay);
-         iconMenu.classList.toggle("_active");
-         menuBody.classList.toggle("_active");
-      }
+  let delay = 500;
+  iconMenu.addEventListener("click", function (e) {
+    iconMenu.classList.toggle("_active");
+    menuBody.classList.toggle("_active");
    });
 };
 
 function menu_close() {
-   let iconMenu = document.querySelector(".icon-menu");
-   let menuBody = document.querySelector(".nav");
+  console.info('клик по ссылке');
    iconMenu.classList.remove("_active");
    menuBody.classList.remove("_active");
 }
+
+// Событие клика по меню
+navLinks.addEventListener('click', menu_close);
+
 /***************************************
 end Бургер меню
+***************************************/
+
+
+/***************************************
+Модальное окно
+***************************************/
+let popupBg = document.querySelector('.modal'); // Фон попап окна
+let popup = document.querySelector('.popup'); // Само окно
+let openPopupButtons = document.querySelectorAll('.open-popup'); // Кнопки для показа окна
+let closePopupButton = document.querySelector('.close-popup'); // Кнопки для скрытия окна
+let closeBtnPopupButton = document.querySelector('.btn-popup'); // Кнопки для скрытия окна
+
+
+//Открытие модального окна при клике на Подписаться
+openPopupButtons.forEach((button) => { // Перебираем все кнопки
+  button.addEventListener('click', (e) => { // Для каждой вешаем обработчик событий на клик
+      e.preventDefault(); // Предотвращаем дефолтное поведение браузера
+      popupBg.classList.add('active'); // Добавляем класс 'active' для фона
+      popup.classList.add('active');
+  })
+});
+
+//Закрытие модального окна при клике на крестик
+closePopupButton.addEventListener('click',() => { // Вешаем обработчик на крестик
+  popupBg.classList.remove('active'); // Убираем активный класс с фона
+  popup.classList.remove('active');
+});
+
+//Закрытие модального окна при клике на OK
+closeBtnPopupButton.addEventListener('click',() => { // Вешаем обработчик на крестик
+  popupBg.classList.remove('active'); // Убираем активный класс с фона
+  popup.classList.remove('active');
+});
+
+//Закрытие модального окна при клике фон
+document.addEventListener('click', (e) => { // Вешаем обработчик на весь документ
+  if(e.target === popupBg) { // Если цель клика - фот, то:
+      popupBg.classList.remove('active'); // Убираем активный класс с фона
+      popup.classList.remove('active');
+  }
+});
+
+//Закрытие модального окна по клавише ESC
+document.addEventListener('keyup', (e) => { // Вешаем обработчик на весь документ
+  if(e.key === 'Escape') { // Если нажата клавиша ESC, то:
+      popupBg.classList.remove('active'); // Убираем активный класс с фона
+      popup.classList.remove('active');
+  }
+});
+/***************************************
+end Модальное окно
 ***************************************/
 
 
